b85b8cd9f230d33ea01ac90664f8b804ae0d9002
rohansaini01111 <rohansa012@gmail.com>
2026-03-14 23:59:59 +0530

    Update vocab.js

--- vocab.js
+++ vocab.js
@@ -1,1 +1,22 @@
+const vocabulary = {
 
+lesson1: [
+
+{jp:"わたし", romaji:"watashi", en:"I"},
+{jp:"わたしたち", romaji:"watashitachi", en:"we"},
+{jp:"あなた", romaji:"anata", en:"you"},
+{jp:"あのひと", romaji:"ano hito", en:"that person"},
+{jp:"あのかた", romaji:"ano kata", en:"that person (polite)"},
+{jp:"みなさん", romaji:"minasan", en:"everyone"},
+{jp:"せんせい", romaji:"sensei", en:"teacher"},
+{jp:"がくせい", romaji:"gakusei", en:"student"},
+{jp:"かいしゃいん", romaji:"kaishain", en:"company employee"},
+{jp:"しゃいん", romaji:"shain", en:"employee"},
+{jp:"ぎんこういん", romaji:"ginkouin", en:"bank employee"},
+{jp:"いしゃ", romaji:"isha", en:"doctor"},
+{jp:"けんきゅうしゃ", romaji:"kenkyuusha", en:"researcher"},
+{jp:"エンジニア", romaji:"enjinia", en:"engineer"}
+
+]
+
+}
