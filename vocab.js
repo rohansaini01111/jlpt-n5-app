e841ed80794ffa91239374d4569a7d6c64a1b314
rohansaini01111 <rohansa012@gmail.com>
2026-03-15 10:05:05 +0530

    Update vocab.js

--- vocab.js
+++ vocab.js
@@ -1,6 +1,8 @@
 const vocabulary = {
 
-lesson1: [
+lesson1: {
+
+main: [
 
 {jp:"わたし", romaji:"watashi", en:"I"},
 {jp:"わたしたち", romaji:"watashitachi", en:"we"},
@@ -8,7 +10,12 @@ lesson1: [
 {jp:"あのひと", romaji:"ano hito", en:"that person"},
 {jp:"あのかた", romaji:"ano kata", en:"that person (polite)"},
 {jp:"みなさん", romaji:"minasan", en:"everyone"},
+{jp:"～さん", romaji:"~san", en:"Mr / Ms"},
+{jp:"～ちゃん", romaji:"~chan", en:"child suffix"},
+{jp:"～くん", romaji:"~kun", en:"boy suffix"},
+{jp:"～じん", romaji:"~jin", en:"nationality"},
 {jp:"せんせい", romaji:"sensei", en:"teacher"},
+{jp:"きょうし", romaji:"kyoushi", en:"instructor"},
 {jp:"がくせい", romaji:"gakusei", en:"student"},
 {jp:"かいしゃいん", romaji:"kaishain", en:"company employee"},
 {jp:"しゃいん", romaji:"shain", en:"employee"},
@@ -16,9 +23,28 @@ lesson1: [
 {jp:"いしゃ", romaji:"isha", en:"doctor"},
 {jp:"けんきゅうしゃ", romaji:"kenkyuusha", en:"researcher"},
 {jp:"エンジニア", romaji:"enjinia", en:"engineer"}
-{jp:"だれ", romaji:"dare", en:"who"},
-{jp:"どなた", romaji:"donata", en:"who (polite)"}
+
+],
+
+extra: [
+
+{jp:"アメリカ", romaji:"amerika", en:"America"},
+{jp:"イギリス", romaji:"igirisu", en:"United Kingdom"},
+{jp:"インド", romaji:"indo", en:"India"},
+{jp:"インドネシア", romaji:"indonesia", en:"Indonesia"},
+{jp:"かんこく", romaji:"kankoku", en:"Korea"},
+{jp:"ちゅうごく", romaji:"chuugoku", en:"China"},
+{jp:"ドイツ", romaji:"doitsu", en:"Germany"},
+{jp:"にほん", romaji:"nihon", en:"Japan"},
+{jp:"ブラジル", romaji:"burajiru", en:"Brazil"},
+{jp:"タイ", romaji:"tai", en:"Thailand"},
+{jp:"フィリピン", romaji:"firipin", en:"Philippines"},
+{jp:"ベトナム", romaji:"betonamu", en:"Vietnam"},
+{jp:"マレーシア", romaji:"mareeshia", en:"Malaysia"}
 
 ]
 
 }
+
+}
+
