c5fbb5762d6ea8a32269f20153ffb4fe95444646
rohansaini01111 <rohansa012@gmail.com>
2026-03-15 22:27:28 +0530

    Update lesson1.js

--- lesson-data/lesson1.js
+++ lesson-data/lesson1.js
@@ -1,1 +1,8 @@
-
+const lesson1 = {
+main:[
+{jp:"わたし",romaji:"watashi",en:"I"},
+{jp:"あなた",romaji:"anata",en:"you"},
+{jp:"あのひと",romaji:"ano hito",en:"that person"},
+{jp:"みなさん",romaji:"minasan",en:"everyone"}
+]
+}
