b154169d1ce7a2ed676a4fd2b59385b123423a30
rohansaini01111 <rohansa012@gmail.com>
2026-03-15 19:16:16 +0530

    Update vocab.js

--- vocab.js
+++ vocab.js
@@ -1,1 +1,84 @@
+const vocabulary = {
 
+lesson1: {
+
+main: [
+
+{jp:"わたし", romaji:"watashi", en:"I"},
+{jp:"あなた", romaji:"anata", en:"you"},
+{jp:"あのひと", romaji:"ano hito", en:"that person"},
+{jp:"あのかた", romaji:"ano kata", en:"that person polite"},
+{jp:"みなさん", romaji:"minasan", en:"everyone"},
+{jp:"〜さん", romaji:"san", en:"Mr/Ms"},
+{jp:"〜ちゃん", romaji:"chan", en:"child suffix"},
+{jp:"〜くん", romaji:"kun", en:"boy suffix"},
+{jp:"〜じん", romaji:"jin", en:"nationality"},
+{jp:"せんせい", romaji:"sensei", en:"teacher"},
+{jp:"きょうし", romaji:"kyoushi", en:"instructor"},
+{jp:"がくせい", romaji:"gakusei", en:"student"},
+{jp:"かいしゃいん", romaji:"kaishain", en:"company employee"},
+{jp:"しゃいん", romaji:"shain", en:"employee"},
+{jp:"ぎんこういん", romaji:"ginkouin", en:"bank employee"},
+{jp:"いしゃ", romaji:"isha", en:"doctor"},
+{jp:"けんきゅうしゃ", romaji:"kenkyuusha", en:"researcher"},
+{jp:"エンジニア", romaji:"enjinia", en:"engineer"}
+
+],
+
+extra: [
+
+{jp:"アメリカ", romaji:"amerika", en:"America"},
+{jp:"イギリス", romaji:"igirisu", en:"United Kingdom"},
+{jp:"インド", romaji:"indo", en:"India"},
+{jp:"インドネシア", romaji:"indonesia", en:"Indonesia"},
+{jp:"かんこく", romaji:"kankoku", en:"Korea"},
+{jp:"ちゅうごく", romaji:"chuugoku", en:"China"},
+{jp:"ドイツ", romaji:"doitsu", en:"Germany"},
+{jp:"にほん", romaji:"nihon", en:"Japan"},
+{jp:"ブラジル", romaji:"burajiru", en:"Brazil"},
+{jp:"タイ", romaji:"tai", en:"Thailand"},
+{jp:"フィリピン", romaji:"firipin", en:"Philippines"},
+{jp:"ベトナム", romaji:"betonamu", en:"Vietnam"},
+{jp:"マレーシア", romaji:"mareeshia", en:"Malaysia"}
+
+]
+
+},
+
+lesson2: {
+
+main: [
+
+{jp:"これ", romaji:"kore", en:"this"},
+{jp:"それ", romaji:"sore", en:"that"},
+{jp:"あれ", romaji:"are", en:"that over there"},
+{jp:"この", romaji:"kono", en:"this (before noun)"},
+{jp:"その", romaji:"sono", en:"that (before noun)"},
+{jp:"あの", romaji:"ano", en:"that over there (before noun)"},
+
+{jp:"ほん", romaji:"hon", en:"book"},
+{jp:"じしょ", romaji:"jisho", en:"dictionary"},
+{jp:"ざっし", romaji:"zasshi", en:"magazine"},
+{jp:"しんぶん", romaji:"shinbun", en:"newspaper"},
+{jp:"ノート", romaji:"nooto", en:"notebook"},
+{jp:"てちょう", romaji:"techou", en:"memo pad"},
+{jp:"めいし", romaji:"meishi", en:"business card"},
+{jp:"カード", romaji:"kaado", en:"card"},
+{jp:"テレホンカード", romaji:"terehon kaado", en:"telephone card"},
+
+{jp:"えんぴつ", romaji:"enpitsu", en:"pencil"},
+{jp:"ボールペン", romaji:"boorupen", en:"ballpoint pen"},
+{jp:"シャープペンシル", romaji:"shaapupenshiru", en:"mechanical pencil"},
+
+{jp:"かぎ", romaji:"kagi", en:"key"},
+{jp:"とけい", romaji:"tokei", en:"watch"},
+{jp:"かさ", romaji:"kasa", en:"umbrella"},
+{jp:"かばん", romaji:"kaban", en:"bag"}
+
+],
+
+extra: []
+
+}
+
+}
